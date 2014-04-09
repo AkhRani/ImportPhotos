b9d9ca93bdeca27aad3f62c79f80352a3e5baad6
Kevin Hart <kevin.kevinhart@gmail.com>
2014-04-08 21:04:50 -0700

    Further Development
    Preferences are closer to being used to remember recent folders
    Looks like ImageMetadataReader is working correctly
    Committing third-party jars, along with license

--- src/com/booleangold/import_photos/ImportPhotos.java
+++ src/com/booleangold/import_photos/ImportPhotos.java
@@ -1,9 +1,19 @@
 package com.booleangold.import_photos;
 
 import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+
 import javax.swing.*;
 
 import java.awt.event.*;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.*;
+import com.drew.metadata.exif.ExifIFD0Directory;
 
 public class ImportPhotos extends JFrame {
     private final JComboBox mSource;
@@ -12,17 +22,19 @@ public class ImportPhotos extends JFrame {
     private final JButton mBrowseDestButton;
     private final JButton mImportButton;
     private final JButton mCancelButton;
+    private static final String SOURCE_KEY = "source_dir_";
+    private static final String DEST_KEY = "source_dir_";
 
     static final long serialVersionUID = 1;
 
     ImportPhotos(String title) {
         setTitle(title);
 
-        //Create a pane and add components to it.
+        // Create Components
+        Preferences prefs = Preferences.userNodeForPackage(ImportPhotos.class);
+
+        mSource = MakeComboBox(prefs, "E:\\DCIM\\", SOURCE_KEY);
         JLabel sourceLabel = new JLabel("Import pictures from: ");
-        mSource = new JComboBox();
-        mSource.setPrototypeDisplayValue("E:\\DCIM\\");
-        mSource.addItem("E:\\DCIM\\");
         mBrowseSourceButton = new JButton("Browse");
         mBrowseSourceButton.addActionListener(new ActionListener() {
             @Override
@@ -31,10 +43,9 @@ public class ImportPhotos extends JFrame {
             }
         });
 
+
         JLabel destLabel = new JLabel("Copy pictures to: ");
-        mDest = new JComboBox();
-        mDest.setPrototypeDisplayValue("D:\\Users\\Beth\\Documents\\Pictures");
-        mDest.addItem("D:\\Users\\Beth\\Documents\\Pictures");
+        mDest = MakeComboBox(prefs, "D:\\Users\\Beth\\Documents\\Pictures", DEST_KEY);
         mBrowseDestButton = new JButton("Browse");
         mBrowseDestButton.addActionListener(new ActionListener() {
             @Override
@@ -59,6 +70,7 @@ public class ImportPhotos extends JFrame {
             }
         });
 
+        //Create a layout and add components to it.
         GroupLayout layout = new GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
 
@@ -99,11 +111,36 @@ public class ImportPhotos extends JFrame {
         this.setResizable(false);
     }
 
+    JComboBox MakeComboBox(Preferences prefs, String defaultItem, String base_key) {
+        JComboBox retval = new JComboBox();
+
+        try {
+            String[] keys = prefs.keys();
+            for (String key : keys) {
+                retval.addItem(prefs.get(key, "Missing Preference"));
+            }
+        }
+        catch (BackingStoreException e) {
+            // TODO
+        }
+        if (retval.getItemCount() == 0) {
+            retval.addItem(defaultItem);
+        }
+        return retval;
+    }
+
+    void SaveComboBox(JComboBox cb, Preferences prefs, String base_key) {
+        int itemCount = cb.getItemCount();
+        for (int i = 0; i < itemCount; i++) {
+            prefs.put(base_key + Integer.toString(i), (String) cb.getItemAt(i));
+        }
+    }
+
     void ChooseDirectory(JComboBox comboBox) {
         JFileChooser chooser;
         // Start at currently selected directory, if possible
         try {
-            chooser = new JFileChooser((String) comboBox.getItemAt(0));
+            chooser = new JFileChooser((String) comboBox.getSelectedItem());
         }
         catch (Exception e) {
             chooser = new JFileChooser();
@@ -136,7 +173,7 @@ public class ImportPhotos extends JFrame {
             return;
         }
         try {
-            RecursiveImport(sourceDir, destRoot);
+            ImportDirectory(sourceDir, destRoot);
         }
         catch (Exception e) {
             // TODO: Error dialog
@@ -144,14 +181,33 @@ public class ImportPhotos extends JFrame {
         }
     }
 
-    void RecursiveImport(File sourceDir, String destRoot) {
+    void ImportDirectory(File sourceDir, String destRoot) {
         for (File file : sourceDir.listFiles()) {
-            System.out.println("Hello " + file.getAbsolutePath());
             if (file.isDirectory()) {
-                RecursiveImport(file, destRoot);
+                ImportDirectory(file, destRoot);
             }
             else {
+                ImportFile(file);
             }
+        }
+    }
+
+    void ImportFile(File file) {
+        try {
+            Metadata meta = ImageMetadataReader.readMetadata(file);
+            Directory exif = meta.getDirectory(ExifIFD0Directory.class);
+            Date date = exif.getDate(ExifIFD0Directory.TAG_DATETIME);
+            System.out.println("Hello " + file.getAbsolutePath() + " " + date.toString());
+        }
+        catch (ImageProcessingException e) {
+            if (file.getName().endsWith(".mov") || file.getName().endsWith(".MOV")) {
+                SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+                System.out.println("Hello " + file.getAbsolutePath() + " " + sdf.format(file.lastModified()));
+            }
+           // TODO
+        }
+        catch (IOException e) {
+            // TODO
         }
     }
 
