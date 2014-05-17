781528b6843aa7a897a3faa26e148d737dfa31be
Kevin Hart <kevin.kevinhart@gmail.com>
2014-05-17 15:06:15 -0700

    Refactored, basically working
    Moved CopyTask class into its own file.
    Now using Apache commons to copy files, preserving modification date.
    Basic functionality works, need to polish button and progress bar
    behavior, get "Cancel" working.

---
+++ src/com/booleangold/import_photos/CopyTask.java
@@ -0,0 +1,112 @@
+package com.booleangold.import_photos;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.SwingWorker;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
+
+class CopyTask extends SwingWorker<Void, Void> {
+    private static final SimpleDateFormat mYearFormat = new SimpleDateFormat("yyyy");
+    private static final SimpleDateFormat mMonthFormat = new SimpleDateFormat("MM");
+
+    private final File mSource;
+    private final String mDestRoot;
+
+    CopyTask(File source, String destRoot) {
+        mSource = source;
+        mDestRoot = destRoot;
+    }
+
+    @Override
+    protected Void doInBackground() throws Exception {
+        try {
+            List<File> filesToImport = CollectDirectory(mSource, mDestRoot);
+            int total = filesToImport.size();
+            firePropertyChange("fileCount", 0, total);
+
+            int count = 0;
+            for (File file : filesToImport) {
+                count++;
+                ImportFile(file);
+                setProgress(count * 100 / total);
+            }
+        }
+        catch (Exception e) {
+            // TODO: Error dialog
+            System.out.println("Failed to traverse directories" + e);
+        }
+        return null;
+    }
+
+    List<File> CollectDirectory(File sourceDir, String destRoot) {
+        LinkedList<File> result = new LinkedList<File>();
+        for (File file : sourceDir.listFiles()) {
+            if (file.isDirectory()) {
+                result.addAll(CollectDirectory(file, destRoot));
+            }
+            else {
+                System.out.println("File: " + file);
+                result.add(file);
+            }
+        }
+        return result;
+    }
+
+    void ImportFile(File file) {
+        String year = null;
+        String month = null;
+
+        try {
+            Metadata meta = ImageMetadataReader.readMetadata(file);
+            Directory exif = meta.getDirectory(ExifSubIFDDirectory.class);
+            Date date = exif.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
+            year = mYearFormat.format(date);
+            month = mMonthFormat.format(date);
+        }
+        catch (ImageProcessingException e) {
+            if (file.getName().endsWith(".mov") || file.getName().endsWith(".MOV")) {
+                year = mYearFormat.format(file.lastModified());
+                month = mMonthFormat.format(file.lastModified());
+            }
+           // TODO
+        }
+        catch (IOException e) {
+            // TODO
+        }
+
+        if (year != null && month != null) {
+            File yearDir = new File(mDestRoot, year);
+            File monthDir = new File(yearDir, month);
+            if (!monthDir.exists()) {
+                monthDir.mkdirs();
+            }
+            // TODO:  Copy file to monthDir
+            File destFile = new File(monthDir, file.getName());
+            if (destFile.exists()) {
+                System.out.println("Skipping duplicate file " + destFile.getPath());
+            }
+            else {
+                System.out.println("Preparing to import " + file.getPath() + " to " + destFile.getPath());
+                try {
+                    FileUtils.copyFile(file, destFile, true);
+                }
+                catch (Exception e) {
+                    // TODO: Error dialog
+                    System.out.println("Failed to import file:  " + e);
+                }
+            }
+        }
+    }
+}
--- src/com/booleangold/import_photos/ImportPhotos.java
+++ src/com/booleangold/import_photos/ImportPhotos.java
@@ -1,10 +1,6 @@
 package com.booleangold.import_photos;
 
 import java.io.*;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.prefs.Preferences;
 
 import javax.swing.*;
@@ -13,20 +9,12 @@ import java.awt.event.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
-import com.drew.imaging.ImageMetadataReader;
-import com.drew.imaging.ImageProcessingException;
-import com.drew.metadata.*;
-import com.drew.metadata.exif.ExifSubIFDDirectory;
-
 public class ImportPhotos extends JFrame
                           implements ActionListener, PropertyChangeListener {
     static final long serialVersionUID = 1;
     private static final String SOURCE_KEY = "source_dir_";
     private static final String DEST_KEY = "dest_dir_";
     private static final int RECENT_DIR_COUNT = 4;
-
-    private static final SimpleDateFormat mYearFormat = new SimpleDateFormat("yyyy");
-    private static final SimpleDateFormat mMonthFormat = new SimpleDateFormat("MM");
 
     private final JComboBox mSource;
     private final JButton mBrowseSourceButton;
@@ -37,30 +25,6 @@ public class ImportPhotos extends JFrame
     private final JProgressBar mProgressBar;
     private final Preferences mPrefs;
     private CopyTask mCopyTask;
-
-    class CopyTask extends SwingWorker<Void, Void> {
-        private final File mSource;
-        private final String mDestRoot;
-
-        CopyTask(File source, String destRoot) {
-            mSource = source;
-            mDestRoot = destRoot;
-        }
-
-        @Override
-        protected Void doInBackground() throws Exception {
-            try {
-                List<File> filesToImport = CollectDirectory(mSource, mDestRoot);
-                // System.out.println("Importing files: " + filesToImport.size());
-                firePropertyChange("fileCount", 0, filesToImport.size());
-            }
-            catch (Exception e) {
-                // TODO: Error dialog
-                System.out.println("Failed to traverse directories");
-            }
-            return null;
-        }
-    }
 
     ImportPhotos(String title) {
         setTitle(title);
@@ -222,100 +186,6 @@ public class ImportPhotos extends JFrame
         mCopyTask.execute();
     }
 
-    List<File> CollectDirectory(File sourceDir, String destRoot) {
-        LinkedList<File> result = new LinkedList<File>();
-        for (File file : sourceDir.listFiles()) {
-            if (file.isDirectory()) {
-                result.addAll(CollectDirectory(file, destRoot));
-            }
-            else {
-                System.out.println("File: " + file);
-                result.add(file);
-            }
-        }
-        return result;
-    }
-
-    File CollectFile(File file, String destRoot) {
-        String year = null;
-        String month = null;
-
-        try {
-            Metadata meta = ImageMetadataReader.readMetadata(file);
-            Directory exif = meta.getDirectory(ExifSubIFDDirectory.class);
-            Date date = exif.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
-            year = mYearFormat.format(date);
-            month = mMonthFormat.format(date);
-        }
-        catch (ImageProcessingException e) {
-            if (file.getName().endsWith(".mov") || file.getName().endsWith(".MOV")) {
-                year = mYearFormat.format(file.lastModified());
-                month = mMonthFormat.format(file.lastModified());
-            }
-           // TODO
-        }
-        catch (IOException e) {
-            // TODO
-        }
-
-        if (year != null && month != null) {
-            File yearDir = new File(destRoot, year);
-            File monthDir = new File(yearDir, month);
-            if (!monthDir.exists()) {
-                monthDir.mkdirs();
-            }
-            // TODO:  Copy file to monthDir
-            File destFile = new File(monthDir, file.getName());
-            if (destFile.exists()) {
-                System.out.println("Skipping duplicate file " + destFile.getPath());
-            }
-            else {
-                System.out.println("Preparing to import " + file.getPath() + " to " + destFile.getPath());
-                // return new File(file, destFile);
-            }
-        }
-        return null;
-    }
-
-    /*
-     *
-                InputStream is = null;
-                OutputStream os = null;
-                try {
-                    is = new FileInputStream(file);
-                    os = new FileOutputStream(destFile);
-                    int length;
-                    while ((length = is.read(mBuffer)) > 0) {
-                        os.write(mBuffer, 0, length);
-                    }
-                    System.out.println("Successfully copied " + file.getPath() + " to " + destFile.getPath());
-                } catch (FileNotFoundException e) {
-                    System.out.println("Failed to copy " + file.getPath() + " to " + destFile.getPath());
-                } catch (IOException e) {
-                    System.out.println("Failed to copy " + file.getPath() + " to " + destFile.getPath());
-                } finally {
-                    try {
-                        is.close();
-                    } catch (IOException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                    try {
-                        os.close();
-                    } catch (IOException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                }
-
-     */
-    public static void main(String[] args) {
-       ImportPhotos window = new ImportPhotos("Import Photos");
-
-       window.setVisible(true);
-       window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         if ("progress" == evt.getPropertyName()) {
@@ -323,10 +193,7 @@ public class ImportPhotos extends JFrame
             mProgressBar.setValue(progress);
         }
         else if ("fileCount" == evt.getPropertyName()) {
-            int count = (Integer) evt.getNewValue();
-            System.out.println("Importing files: " + count);
             mProgressBar.setIndeterminate(false);
-            mProgressBar.setMaximum(count);
         }
     }
 
@@ -335,4 +202,11 @@ public class ImportPhotos extends JFrame
         // TODO Auto-generated method stub
 
     }
+
+    public static void main(String[] args) {
+        ImportPhotos window = new ImportPhotos("Import Photos");
+
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     }
 }
